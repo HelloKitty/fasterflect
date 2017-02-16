edd5f3ac95eb732b13260b2c481afe7a8e23e91c
HelloKitty <andrew.blakely@ymail.com>
2017-02-16 05:12:21 -0600

    Added Myget Build

--- src/Fasterflect/Properties/AssemblyInfo.cs
+++ src/Fasterflect/Properties/AssemblyInfo.cs
@@ -6,9 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Andrew Blakely")]
 [assembly: AssemblyProduct("Fasterflect")]
 [assembly: AssemblyTrademark("")]
+[assembly: InternalsVisibleTo("Fasterflect.Tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
