6f918004c311c3fc4f239b6dc9e23a2542de2e24
Daniil Sokolyuk <d.sokolyuk@rambler-co.ru>
2018-01-06 02:36:25 +0300

    Strong named assembly

--- src/Fasterflect.Netstandard/Properties/AssemblyInfo.cs
+++ src/Fasterflect.Netstandard/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("www.github.com/hellokitty")]
 [assembly: AssemblyProduct("Fasterflect")]
 [assembly: AssemblyTrademark("")]
-[assembly: InternalsVisibleTo("Fasterflect.Netframework.Tests")]
+[assembly: InternalsVisibleTo("Fasterflect.Netframework.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010065ab7e46a5e0c5ccfef6d646e1848f47dc001d9df8d7b17765bd799fc953d37fa37acb0bd89f9627709d25ab5b85f46aaba417e2f8eb189a1848c65c08164c50519112c131c4db1a6845204890e1dca1e61ee2928e4fa6b8e7b3f3b587961ef2b7751dce99d2ad6eef5d8959dd80e45d384527af6e4679ea4a512ea573b4c1bc")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
